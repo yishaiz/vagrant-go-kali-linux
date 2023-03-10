ecf98a10291031c375c10554cbb3f34a0a86866b
yishai <yishai5555@gmail.com>
2023-03-10 12:11:29 +0200

    02-fix-go-installation-for-kali

--- provision-go.sh
+++ provision-go.sh
@@ -1,29 +1,33 @@
-#!/bin/bash
-# This is the entry point for configuring the system.
-#####################################################
+apt-get install -y golang-go
 
-#install basic tools
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install git
 
-#get golang 1.9.1
-curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
 
-#unzip the archive 
-tar -xvf go1.9.1.linux-amd64.tar.gz
+# #!/bin/bash
+# # This is the entry point for configuring the system.
+# #####################################################
 
-#move the go lib to local folder
-mv go /usr/local
+# #install basic tools
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install git
 
-#delete the source file
-rm  go1.9.1.linux-amd64.tar.gz
+# #get golang 1.9.1
+# curl -O https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
 
-#only full path will work
-touch /home/vagrant/.bash_profile
+# #unzip the archive 
+# tar -xvf go1.9.1.linux-amd64.tar.gz
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> /home/vagrant/.bash_profile
+# #move the go lib to local folder
+# mv go /usr/local
 
-echo `export GOPATH=/home/vagrant/workspace:$PATH` >> /home/vagrant/.bash_profile
+# #delete the source file
+# rm  go1.9.1.linux-amd64.tar.gz
 
-export GOPATH=/home/vagrant/workspace
+# #only full path will work
+# touch /home/vagrant/.bash_profile
 
-mkdir -p "$GOPATH/bin" 
+# echo "export PATH=$PATH:/usr/local/go/bin" >> /home/vagrant/.bash_profile
+
+# echo `export GOPATH=/home/vagrant/workspace:$PATH` >> /home/vagrant/.bash_profile
+
+# export GOPATH=/home/vagrant/workspace
+
+# mkdir -p "$GOPATH/bin" 
